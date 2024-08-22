a8fc70b4774b7d2f9e02f366f2780577b8080ca0
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-22 13:38:45 +0300

    Add LoginManager and create a requierements.txt file

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,8 +4,9 @@ Initialize the flask app
 """
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
@@ -15,5 +16,11 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login_manager = LoginManager(app)
+
+cors = CORS(app, resources={
+    r"/*": {"origins": "*"}
+})
 
 from backend import models, routes
--- backend/config.py
+++ backend/config.py
@@ -8,5 +8,5 @@ import os
 class Config:
     """Configuration class for the flask application"""
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "TODO")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "** TODO **")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
