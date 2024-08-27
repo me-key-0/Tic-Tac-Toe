461fb9252a4c8389808e200b3e4787c991d4d36b
Juweria86 <jujumelmi143@gmail.com>
2024-08-27 23:53:05 +0300

    implemented user auth routes

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -7,6 +7,8 @@ from flask_cors import CORS
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_session import Session
 
 from src.config import Config
 
@@ -18,6 +20,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 login_manager = LoginManager(app)
+bcrypt = Bcrypt(app) 
+login_manager = LoginManager(app)
+server_session = Session(app)
 
 cors = CORS(app, resources={
     r"/*": {"origins": "*"}
